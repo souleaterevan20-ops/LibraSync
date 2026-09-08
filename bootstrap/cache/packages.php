8efcefb149cad8d05fa4211644fc859054294e2a
Mitch Lombreno <lombrenomitch@gmail.com>
2026-09-09 03:51:31 +0800

    Clear auto-discovery cache map files

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,13 +6,6 @@
       0 => 'Laravel\\Breeze\\BreezeServiceProvider',
     ),
   ),
-  'laravel/pail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Pail\\PailServiceProvider',
-    ),
-  ),
   'laravel/sail' => 
   array (
     'providers' => 
